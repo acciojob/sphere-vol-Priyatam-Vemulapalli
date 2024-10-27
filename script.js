d26389ce70bcf8717979ce957b5aff413c0c84ce
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-27 11:16:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function volume_sphere() {
     //Write your code here
+	
+	event.preventDefault(); 
+
+    const radiusInput = document.getElementById("radius").value;
+    const volumeOutput = document.getElementById("volume");
+
+    
+    const radius = parseFloat(radiusInput);
+
+    if (isNaN(radius) || radius < 0) {
+        volumeOutput.value = "NaN";
+    } else {
+        const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        
+        volumeOutput.value = volume.toFixed(4);
+    }
   
 } 
 
